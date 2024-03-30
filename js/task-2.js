f67a26022a7e4e29c62a7657981c2c56090e21f7
damian-sirenko <damian.sirenko@gmail.com>
2024-03-30 09:12:11 +0100

    hw 3 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,27 @@
-function makeArray(firstArray, secondArray, maxLength) {
-	const finalArray = firstArray.concat(secondArray);
-	if (finalArray.length >= maxLength) {
-		return finalArray.slice(0, maxLength);
-	} else {
-				return finalArray;
-			}
-}
-		
+class Storage {
+	#items;
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+	  constructor(items) {
+    this.#items = items;
+  }
+	getItems() {
+		return this.#items;
+	}
+	addItem(newItem) {
+		return this.#items.push(newItem);
+	}
+	removeItem(itemToRemove) {
+		const itemToRemoveIndex = this.#items.indexOf(itemToRemove);
+		if (itemToRemoveIndex !== -1) {
+			this.#items.splice(itemToRemoveIndex, 1)
+		}
+	}
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
